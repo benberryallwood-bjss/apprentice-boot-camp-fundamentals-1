3f74ee6fdfa20d2d9b57efd2f0bf094af5a879ed
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-15 14:59:05 +0000

    Remove logic from Cards which is now in PlayingCard

--- exercises/java/src/main/java/cards/Cards.java
+++ exercises/java/src/main/java/cards/Cards.java
@@ -21,35 +21,6 @@ public class Cards {
 
         int cardNumber = 0;
         for (PlayingCard card : deck) {
-            /*String faceValueName;
-            switch (card[1]){
-                case 0: faceValueName = "ace"; break;
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9: faceValueName = Integer.toString(card[1]+1); break;
-                case 10: faceValueName = "jack"; break;
-                case 11: faceValueName = "queen"; break;
-                case 12: faceValueName = "king"; break;
-                default: throw new IllegalArgumentException("Something went wrong " + card[1] + "is not a valid faceValue!");
-            }
-
-            String suitName;
-            switch (card[0]){
-                case 0: suitName = "clubs"; break;
-                case 1: suitName = "diamonds"; break;
-                case 2: suitName = "hearts"; break;
-                case 3: suitName = "spades"; break;
-                default: throw new IllegalArgumentException("Something went wrong " + card[0] + "is not a valid suitName!");
-            }
-
-             */
-
             result[cardNumber] = card.toString();
             cardNumber++;
         }
