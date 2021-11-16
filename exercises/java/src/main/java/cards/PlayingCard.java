6d7c062e856f1097e29cb6180202c5f3ad687414
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-16 10:17:15 +0000

    Store faceValue as string

--- exercises/java/src/main/java/cards/PlayingCard.java
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -2,28 +2,13 @@ package cards;
 
 public class PlayingCard {
     private final Suit suit;
-    private final int faceValue;
+    private final String faceValue;
 
     public PlayingCard(Suit suit, int faceValue) {
-        if (faceValue < 1 || faceValue > 13) {
-            throw new IllegalArgumentException("faceValue (" + faceValue + ") must be between 1 and 13");
-        }
         this.suit = suit;
-        this.faceValue = faceValue;
-    }
 
-    @Override
-    public String toString() {
-        return faceValueName() + " of " + suitName();
-    }
-
-    private String suitName() {
-        return suit.toString().toLowerCase();
-    }
-
-    private String faceValueName() {
         switch (faceValue) {
-            case 1: return "ace";
+            case 1: this.faceValue = "ace"; break;
             case 2:
             case 3:
             case 4:
@@ -32,11 +17,20 @@ public class PlayingCard {
             case 7:
             case 8:
             case 9:
-            case 10: return "" + faceValue;
-            case 11: return "jack";
-            case 12: return "queen";
-            case 13: return "king";
-            default: return null; // cannot happen because of constructor
+            case 10: this.faceValue = "" + faceValue; break;
+            case 11: this.faceValue = "jack"; break;
+            case 12: this.faceValue = "queen"; break;
+            case 13: this.faceValue = "king"; break;
+            default: throw new IllegalArgumentException("faceValue (" + faceValue + ") must be between 1 and 13");
         }
+    }
+
+    @Override
+    public String toString() {
+        return faceValue + " of " + suitName();
+    }
+
+    private String suitName() {
+        return suit.toString().toLowerCase();
     }
 }
